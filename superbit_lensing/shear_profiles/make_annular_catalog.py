60fa377616791f22c16957bd64da0b834d234cff
Jackson Henry <jackson.henry3@gmail.com>
2025-03-10 10:37:14 -0400

    Revert make_annular_catalog.py

--- superbit_lensing/shear_profiles/make_annular_catalog.py
+++ superbit_lensing/shear_profiles/make_annular_catalog.py
@@ -85,9 +85,9 @@ class AnnularCatalog():
         self.coadd_center = annular_info['coadd_center']
 
         if self.outdir is not None:
-           self.outfile = os.path.join(self.outdir, self.outfile)
+            self.outfile = os.path.join(self.outdir, self.outfile)
         else:
-           self.outdir = ''
+            self.outdir = ''
 
         self.det_cat = Table.read(self.detect_cat, hdu=2)
         self.mcal = Table.read(self.mcal_file)
