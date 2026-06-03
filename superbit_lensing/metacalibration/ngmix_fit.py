fb2afd8681dd2286f4158d8c8fe4306a0654cd54
Sayan Saha <rocks.sayan03@gmail.com>
2026-06-03 19:30:12 -0400

    added galsim fitter

--- superbit_lensing/metacalibration/ngmix_fit.py
+++ superbit_lensing/metacalibration/ngmix_fit.py
@@ -1,6 +1,7 @@
 import ngmix
 from ngmix.medsreaders import NGMixMEDS
 from ngmix.shape import e1e2_to_g1g2
+from ngmix.gaussmom import GaussMom
 import numpy as np
 from astropy.table import Table, Row, vstack, hstack
 import os, sys, time, traceback
@@ -103,40 +104,23 @@ class SuperBITNgmixFitter():
 
     def _get_priors(self, Tmaxval = 1000):
 
-        # This bit is needed for ngmix v2.x.x
-        # won't work for v1.x.x
         rng = np.random.RandomState(self.seed)
-
-        # prior on ellipticity.  The details don't matter, as long
-        # as it regularizes the fit.  This one is from Bernstein & Armstrong 2014
 
         g_sigma = 0.3
         g_prior = ngmix.priors.GPriorBA(g_sigma, rng=rng)
-
-        # 2-d gaussian prior on the center
-        # row and column center (relative to the center of the jacobian, which would be zero)
-        # and the sigma of the gaussians
 
         # units same as jacobian, probably arcsec
         row, col = 0.0, 0.0
         row_sigma, col_sigma = 0.2, 0.2 # a bit smaller than pix size of SuperBIT
         cen_prior = ngmix.priors.CenPrior(row, col, row_sigma, col_sigma, rng=rng)
 
-        # T prior.  This one is flat, but another uninformative you might
-        # try is the two-sided error function (TwoSidedErf)
-
         Tminval = -1.0 # arcsec squared
         T_prior = ngmix.priors.FlatPrior(Tminval, Tmaxval, rng=rng)
-
-        # similar for flux.  Make sure the bounds make sense for
-        # your images
 
         Fminval = -1.e1
         Fmaxval = 1.e5
         F_prior = ngmix.priors.FlatPrior(Fminval, Fmaxval, rng=rng)
 
-        # now make a joint prior.  This one takes priors
-        # for each parameter separately
         priors = ngmix.joint_prior.PriorSimpleSep(
         cen_prior,
         g_prior,
@@ -297,14 +281,14 @@ def mp_fit_one(obslist, prior, rng, psf_model='gauss', gal_model='gauss', mcal_p
     cuts for each shear step (i.e. no_shear,1p,1m,2p,2m).
     """
     # get image pixel scale (assumes constant across list)
-    jacobian = obslist[0]._jacobian
-    Tguess = 4*jacobian.get_scale()**2
+    # jacobian = obslist[0]._jacobian
+    # Tguess = 4*jacobian.get_scale()**2
+    guess_gm = GaussMom(1.2).go(obslist[0])
+    Tguess = guess_gm['T']
+    flux_guess = guess_gm['flux']
     ntry = 20
     lm_pars = {'maxfev':2000, 'xtol':5.0e-5, 'ftol':5.0e-5}
     psf_lm_pars={'maxfev': 4000, 'xtol':5.0e-5,'ftol':5.0e-5}
-
-    fitter = ngmix.fitting.Fitter(model=gal_model, prior=prior, fit_pars=lm_pars)
-    guesser = ngmix.guessers.TPSFFluxAndPriorGuesser(rng=rng, T=Tguess, prior=prior)
 
     # psf fitting
     if 'em' in psf_model:
@@ -319,11 +303,22 @@ def mp_fit_one(obslist, prior, rng, psf_model='gauss', gal_model='gauss', mcal_p
     elif psf_model == 'gauss':
         psf_fitter = ngmix.fitting.Fitter(model='gauss', fit_pars=psf_lm_pars)
         psf_guesser = ngmix.guessers.SimplePSFGuesser(rng=rng)
+    elif psf_model == 'galsim':
+        pass
     else:
-        raise ValueError('psf_model must be one of emn, coellipn, or gauss')
+        raise ValueError('psf_model must be one of emn, coellipn, gauss, or galsim')
+        
 
-    psf_runner = ngmix.runners.PSFRunner(fitter=psf_fitter, guesser=psf_guesser, ntry=ntry)
+    if psf_model =='galsim':
+        guesser = ngmix.guessers.TFluxAndPriorGuesser(rng=rng, T=Tguess, flux=flux_guess, prior=prior)
+        fitter = ngmix.fitting.GalsimFitter(model=gal_model, prior=prior, fit_pars=lm_pars)
+        psf_runner = None
 
+    else:
+        guesser = ngmix.guessers.TPSFFluxAndPriorGuesser(rng=rng, T=Tguess, prior=prior)
+        fitter = ngmix.fitting.Fitter(model=gal_model, prior=prior, fit_pars=lm_pars)
+        psf_runner = ngmix.runners.PSFRunner(fitter=psf_fitter, guesser=psf_guesser, ntry=ntry)
+        
     runner = ngmix.runners.Runner(fitter=fitter, guesser=guesser, ntry=ntry)
 
     types = mcal_pars['types']
@@ -381,7 +376,7 @@ def check_obj_flags(obj, min_cutouts=1):
     return False, None
 
 def mp_run_fit(i, obj, obslist, prior,
-               logprint, rng, psf_model='gauss', gal_model='gauss', mcal_pars= {'psf': 'dilate', 'mcal_shear': 0.01}):
+               logprint, rng, psf_model='gauss', gal_model='gauss', mcal_pars= DEFAULT_MCAL_PARS):
     '''
     parallelized version of original ngmix_fit code
 
